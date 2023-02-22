595083b667d4646d756198170f4ac0668961752b
anneabreu <116192859+anneabreu@users.noreply.github.com>
2023-02-22 11:51:13 -0300

    Update BuscarProduto.java

--- DesafioTecnicoKlokTech/src/main/java/org/tech/klok/BuscarProduto/BuscarProduto.java
+++ DesafioTecnicoKlokTech/src/main/java/org/tech/klok/BuscarProduto/BuscarProduto.java
@@ -15,13 +15,11 @@ import static org.tech.klok.Metodos.DriverFactory.killDriver;
 public class BuscarProduto {
     PageBuscarProduto page = new PageBuscarProduto();
 
-    //Entrando no site da magalu
     @Given("um produto")
     public void umProduto() {
         page.acessarTelaInicial("https://www.magazineluiza.com.br/");
     }
 
-    //Pesquisando por um produto
     @When("busca por um {string}")
     public void buscaPorUm(String produto) {
         page.esperarPor(page.getElementos().getPesquisar());
@@ -29,7 +27,6 @@ public class BuscarProduto {
         page.Pesquisar().sendKeys(Keys.ENTER);
     }
 
-    //Verificando os produtos que foram achados
     @Then("acha um produto")
     public void achaUmProduto() {
         page.esperarPor(page.getElementos().getResultado());
@@ -39,21 +36,18 @@ public class BuscarProduto {
         }
     }
 
-    //2 cenario onde pesquisamos por um produto que nao existe
     @Then("não acha um produto")
     public void nãoAchaUmProduto() {
         page.esperarPor(page.getElementos().getResultadoLista());
         Assert.assertEquals(Constantes.nao_encontra_produto, page.ResultadoLista().getText());
     }
 
-    //3 cenario onde procuramos por um produto com caracteres invalidos
     @Then("não retorna resultados")
     public void nãoRetornaResultados() {
         page.esperarPor(page.getElementos().getResultadoLista());
         Assert.assertEquals(Constantes.nao_retorna_resultados, page.ResultadoLista().getText());
     }
 
-    //teste 4 - selencionamos um produto da pagina de resultados
     @And("clica no produto escolhido")
     public void clicaNoProdutoEscolhido() {
         page.esperarPor(page.getElementos().getResultado());
@@ -61,14 +55,12 @@ public class BuscarProduto {
         page.Resultado().get(1).click();
     }
 
-    //verifica a pagina do produto
     @Then("o sistema deve exibir a pagina do produto")
     public void oSistemaDeveExibirAPaginaDoProduto() {
         page.esperarPor(page.getElementos().getNote());
         Assert.assertEquals(page.Note().getText(), Constantes.verificar);
     }
 
-    //teste 6
     @And("seleciona uma categoria")
     public void selecionaUmaCategoria() {
         page.esperarPor(page.getElementos().getBusque_por());
